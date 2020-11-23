0539d2b6c46f9edfc1da7fd4c531637bb5529366
蓝肖震 <2424611561@qq.com>
2020-11-23 17:30:52 +0800

    1

---
+++ src/main/java/pojo/Studen.java
@@ -0,0 +1,19 @@
+package pojo;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("studen")
+public class Studen {
+    Integer id;   //主键
+    String Name; //名字
+    Double Chinese; //语文
+    Double Math;  //数学
+    Double English; //英语
+    Double History; //地理
+
+
+}
