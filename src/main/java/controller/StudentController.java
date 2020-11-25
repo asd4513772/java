57b8b7c753a294ce243d7917197a9800279be524
蓝肖震 <2424611561@qq.com>
2020-11-25 11:24:23 +0800

    修改2

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -16,29 +16,29 @@ import java.util.List;
 public class StudentController {
     @Autowired
     private StudentService studentService;
-
+    //分页查询，将数据按照分页显示在页面
     @GetMapping("selectAll")
     @ResponseBody
     public List<Student> SelectAindll(Integer x){
         return studentService.selectlimit(x);
     }
-
+    //按照ID查找单个数据
     @GetMapping("selectById")
     @ResponseBody
     public Student SlectById(Integer id){
         return studentService.selectById(id);
     }
-
+    //按照ID删除数据
     @PostMapping("deleteById")
     public void DeleteById(Integer id){
         studentService.deleteById(id);
     }
-
+    //按照ID修改数据
     @PostMapping("updataById")
     public void UpdateById(Integer id){
         studentService.updateById(id);
     }
-
+    //新增数据
     @PostMapping("inserter")
     public Student Inserte(Student student){
         return studentService.insertById(student);
